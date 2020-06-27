fd3603daca1ba5aef8beaed2732d89f490f9631d
Alexander Borovikov <alexander.borovikov@outlook.com>
2020-06-27 17:18:19 +0300

    Update license copyright year

--- Package.swift
+++ Package.swift
@@ -6,17 +6,22 @@ import PackageDescription
 let package = Package(
     name: "SwiftNetwork",
     platforms: [
-        .iOS(.v10),
-        .tvOS(.v10)],
+        .iOS(.v10), .tvOS(.v10)
+    ],
     products: [
         .library(
             name: "SwiftNetwork",
             targets: ["SwiftNetwork"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         .target(
             name: "SwiftNetwork",
-	    path: "Sources")
+            path: "Sources",
+            dependencies: [])
     ],
     swiftLanguageVersions: [.v5]
 )
