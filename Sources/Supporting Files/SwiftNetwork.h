2cadeae4439aadd1e016fad818bcec9fa0d71c56
Alexander Borovikov <alexander.borovikov@outlook.com>
2020-07-01 14:48:05 +0300

    Updated project structure

---
+++ Sources/Supporting Files/SwiftNetwork.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNetwork.h
+//  SwiftNetwork
+//
+//  Created by Alexander Borovikov on 01.07.2020.
+//  Copyright © 2020 Alexander Borovikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftNetwork.
+FOUNDATION_EXPORT double SwiftNetworkVersionNumber;
+
+//! Project version string for SwiftNetwork.
+FOUNDATION_EXPORT const unsigned char SwiftNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNetwork/PublicHeader.h>
+
+
